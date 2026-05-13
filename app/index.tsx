8915d9798519d2a5430c53945940a33ddafb35f0
YavuzTG <yavuzturker@icloud.com>
2026-05-13 03:09:05 +0300

    vercel 5

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  // Uygulama açılınca direkt senin ana ekranına yönlendirecek
-  return <Redirect href="/screens/home.tsx" />; 
+  // .tsx uzantısını sildik, sadece dosya yolunu yazdık
+  return <Redirect href="/screens/home" />; 
 }
